2468465ac78eba7c7c16977883c8c14b47e8605c
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-02-20 23:27:24 -0300

    ref: tirando slides do app.tsx e transferindo para pasta mock

---
+++ src/mocks/slide/index.ts
@@ -0,0 +1,20 @@
+import IMAGES from "../../assets";
+
+const SLIDES = [
+  {
+    key: "1",
+    image: IMAGES.startImages.startTasks,
+    text: "Organize sua vida alinhando suas tarefas",
+  },
+  {
+    key: "2",
+    image: IMAGES.startImages.startFood,
+    text: "Organize sua alimentação com a gente",
+  },
+  {
+    key: "3",
+    image: IMAGES.startImages.startDiary,
+    text: "Desabafe sobre seu dia com nosso querido diário",
+  },
+];
+export default SLIDES;
