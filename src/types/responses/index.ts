52356365de4b9ec0b2b2f628a2d1a1ccede2fc49
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-02-27 23:39:15 -0300

    feat: type DataUser

--- src/types/responses/index.ts
+++ src/types/responses/index.ts
@@ -1,6 +1,15 @@
 export type AuthResponse = {
   params: {
-    access_token: string;
+    access_token: string | any;
   };
   type: string;
+};
+
+export type DataUser = {
+  family_name: string;
+  name: string;
+  picture: string;
+  locale: string;
+  given_name: string;
+  id: string;
 };
