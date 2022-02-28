5132b8dab6ea3ead5adf3ac4f46f8cf6e02ed5d9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-02-27 23:45:46 -0300

    feat: passando token no redux e finalizando o layout

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback } from "react";
-import { Image } from "react-native";
 import * as AuthSession from "expo-auth-session";
 import { useNavigation } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
+import { setDataUser } from "../../redux/slices/auth";
+import { useStorage } from "../../redux/hooks";
 
 import { AuthResponse } from "../../types/responses";
 import { StartSlidePage, Text } from "../../design";
@@ -15,22 +17,35 @@ import {
 import IMAGES from "../../assets";
 
 const SignIn = () => {
+  const dispatch = useDispatch();
+
   const handleSignIn = useCallback(async () => {
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
     const { type, params } = (await AuthSession.startAsync({
       authUrl,
     })) as AuthResponse;
+
+    if (type === "success") {
+      dispatch(setDataUser(params.access_token));
+    }
   }, []);
+
+  const { dataUser } = useStorage();
+
+  console.log(dataUser);
 
   return (
     <>
       <StartSlidePage />
+      <S.ContentSocialLoginImage>
+        <S.SocialLoginImage source={IMAGES.login.socialLogin} />
+      </S.ContentSocialLoginImage>
       <S.Container>
         <S.ContentLogin>
           <S.ContentButtonLogin>
             <S.ButtonLogin onPress={handleSignIn}>
               <S.ContentGoogleImage>
-                <Image source={IMAGES.login.google} />
+                <S.GoogleImage source={IMAGES.login.google} />
               </S.ContentGoogleImage>
               <Text type="h4">Entrar com o google</Text>
             </S.ButtonLogin>
