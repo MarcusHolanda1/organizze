9e40a475c758a02591e056e9b54758728556cbbb
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-06 00:15:04 -0300

    feat: login recebendo dados

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import * as AuthSession from "expo-auth-session";
-import { useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
 import { setDataUser } from "../../redux/slices/auth";
 import { useStorage } from "../../redux/hooks";
@@ -16,8 +15,10 @@ import {
 } from "../../config/google";
 import IMAGES from "../../assets";
 
-const SignIn = () => {
+const SignIn = ({ navigation }: any) => {
   const dispatch = useDispatch();
+
+  const { dataUser } = useStorage();
 
   const handleSignIn = useCallback(async () => {
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
@@ -27,12 +28,27 @@ const SignIn = () => {
 
     if (type === "success") {
       dispatch(setDataUser(params.access_token));
+      navigation.navigate("BottomTabNavigate");
     }
   }, []);
 
-  const { dataUser } = useStorage();
+  const loadProfile = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${dataUser}`
+    );
+    const userInfo = await response.json();
+    console.log("infors aki ", userInfo);
+  };
 
-  console.log(dataUser);
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  useEffect(() => {
+    if (dataUser) {
+      navigation.navigate("BottomTabNavigate");
+    }
+  }, []);
 
   return (
     <>
