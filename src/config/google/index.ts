805755811b57ed20a59df55c92cc674c3d3b3ca7
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-25 16:36:42 -0300

    ref: retirando consts secrets

--- src/config/google/index.ts
+++ src/config/google/index.ts
@@ -1,5 +1,2 @@
-export const CLIENT_ID =
-  "978681433223-jis6e5lsi8h03f5ru0010uklbmf8312v.apps.googleusercontent.com";
-export const REDIRECT_URI = "https://auth.expo.io/@marcus.holanda/organizze";
 export const RESPONSE_TYPE = "token";
 export const SCOPE = encodeURI("profile email");
