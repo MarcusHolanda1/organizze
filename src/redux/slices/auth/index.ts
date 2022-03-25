d09a0dfe858ab1627cfe9e5dc6246ddf1c0b4dc7
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-25 16:39:13 -0300

    ref: alterando reducer

--- src/redux/slices/auth/index.ts
+++ src/redux/slices/auth/index.ts
@@ -3,6 +3,7 @@ import { DataUser, AuthResponse } from "../../../types/responses";
 
 const initialState = {
   dataUser: {} as DataUser,
+  responseAuth: {} as AuthResponse,
   isAuth: false,
 };
 
@@ -10,11 +11,11 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setDataUser: (state, action: PayloadAction<DataUser | AuthResponse>) => {
-      state.dataUser = {
-        ...state.dataUser,
-        ...action.payload,
-      };
+    setResponseLoginUser: (state, action: PayloadAction<AuthResponse>) => {
+      state.responseAuth = action.payload;
+    },
+    setDataUser: (state, action: PayloadAction<DataUser>) => {
+      state.dataUser = action.payload;
       state.isAuth = true;
     },
   },
@@ -22,5 +23,5 @@ const authSlice = createSlice({
 
 const { reducer, actions } = authSlice;
 
-export const { setDataUser } = actions;
+export const { setResponseLoginUser, setDataUser } = actions;
 export default reducer;
