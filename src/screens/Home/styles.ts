6dd6b22338c935c2ddfbfbb7498b30cfd124c965
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-26 17:02:45 -0300

    style: home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,1 +1,10 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { theme } from "../../theme";
+
+export const HelloContent = styled.View`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-bottom: ${RFValue(theme.spacing.n12)}px;
+`;
