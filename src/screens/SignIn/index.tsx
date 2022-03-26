9155702f0b8b547ea75aeff6abdbff20e5a5bc29
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-26 17:00:34 -0300

    feat: .env e melhorias login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,24 +1,31 @@
 import React, { useCallback, useEffect } from "react";
 import * as AuthSession from "expo-auth-session";
 import { useDispatch } from "react-redux";
-import { setDataUser } from "../../redux/slices/auth";
-import { useStorage } from "../../redux/hooks";
+import {
+  setIsAuthFalse,
+  setIsAuthTrue,
+  setResponseLoginUser,
+} from "../../redux/slices/auth";
 
 import { AuthResponse } from "../../types/responses";
 import { StartSlidePage, Text } from "../../design";
 import * as S from "./styles";
-import {
-  CLIENT_ID,
-  REDIRECT_URI,
-  RESPONSE_TYPE,
-  SCOPE,
-} from "../../config/google";
+import { RESPONSE_TYPE, SCOPE } from "../../config/google";
 import IMAGES from "../../assets";
+import { useStorage } from "../../redux/hooks";
 
-const SignIn = ({ navigation }: any) => {
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
+
+const SignIn = () => {
   const dispatch = useDispatch();
+  const { responseAuth } = useStorage();
 
-  const { dataUser } = useStorage();
+  useEffect(() => {
+    if (responseAuth) {
+      dispatch(setIsAuthFalse());
+    }
+  }, []);
 
   const handleSignIn = useCallback(async () => {
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
@@ -27,26 +34,8 @@ const SignIn = ({ navigation }: any) => {
     })) as AuthResponse;
 
     if (type === "success") {
-      dispatch(setDataUser(params.access_token));
-      navigation.navigate("BottomTabNavigate");
-    }
-  }, []);
-
-  const loadProfile = async () => {
-    const response = await fetch(
-      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${dataUser}`
-    );
-    const userInfo = await response.json();
-    console.log("infors aki ", userInfo);
-  };
-
-  useEffect(() => {
-    loadProfile();
-  }, []);
-
-  useEffect(() => {
-    if (dataUser) {
-      navigation.navigate("BottomTabNavigate");
+      dispatch(setResponseLoginUser(params.access_token));
+      dispatch(setIsAuthTrue());
     }
   }, []);
 
