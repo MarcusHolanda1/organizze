e239339a1ebaefa2c51414196763fffe92e1f7b2
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-26 16:59:11 -0300

    feat: load profile infos na home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import { Page } from "../../design";
+import * as S from "./styles";
+import { Page, Text, CardHome } from "../../design";
+import { theme } from "../../theme";
+import { useStorage } from "../../redux/hooks";
+import { setDataUser } from "../../redux/slices/auth";
 
 const Home = () => {
+  const { responseAuth, dataUser } = useStorage();
+  const dispatch = useDispatch();
+
+  const loadProfile = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${responseAuth}`
+    );
+    const userInfo = await response.json();
+    dispatch(setDataUser(userInfo));
+  };
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
   return (
     <Page>
-      <Text>Olaaa</Text>
+      <S.HelloContent>
+        <Text type="h3" color={theme.colors.primaryBold}>
+          Olá, {dataUser?.given_name}
+        </Text>
+      </S.HelloContent>
+      <CardHome>
+        <Text type="h1">STroo</Text>
+      </CardHome>
     </Page>
   );
 };
