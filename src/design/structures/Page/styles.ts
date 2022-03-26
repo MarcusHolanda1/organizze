bf4305cc5ea62eeb3ef72885716b344fd6160395
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-03-26 16:52:44 -0300

    ref: utilizando theme spacing

--- src/design/structures/Page/styles.ts
+++ src/design/structures/Page/styles.ts
@@ -2,11 +2,12 @@ import styled from "styled-components/native";
 import { StatusBar, Platform } from "react-native";
 
 import { theme } from "../../../theme";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.SafeAreaView`
   padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
 `;
 
 export const Content = styled.View`
-  padding: ${theme.spacing.n16};
+  padding: ${RFValue(theme.spacing.n16)}px;
 `;
