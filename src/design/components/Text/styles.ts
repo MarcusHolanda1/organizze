d8e3cc1296ae2d5c5fed81c3034a8eea179024e9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-07-17 20:32:33 -0300

    feat: adding fontFamily prop to text

--- src/design/components/Text/styles.ts
+++ src/design/components/Text/styles.ts
@@ -4,34 +4,36 @@ import { theme } from "../../../theme";
 
 interface Text {
   color: string;
+  fontFamily: string;
 }
 
 export const H1 = styled.Text<Text>`
   color: ${(props) => props.color};
+
   font-size: ${RFValue(36)}px;
-  font-family: ${theme.fonts.bold.fontFamily};
+  font-family: ${(props) => props.fontFamily};
 `;
 
 export const H3 = styled.Text<Text>`
   color: ${(props) => props.color};
   font-size: ${RFValue(24)}px;
-  font-family: ${theme.fonts.regular.fontFamily};
+  font-family: ${(props) => props.fontFamily};
 `;
 
 export const H4 = styled.Text<Text>`
   color: ${(props) => props.color};
   font-size: ${RFValue(18)}px;
-  font-family: ${theme.fonts.bold.fontFamily};
+  font-family: ${(props) => props.fontFamily};
 `;
 
 export const P = styled.Text<Text>`
   color: ${(props) => props.color};
   font-size: ${RFValue(16)}px;
-  font-family: ${theme.fonts.regular.fontFamily};
+  font-family: ${(props) => props.fontFamily};
 `;
 
 export const SMALL = styled.Text<Text>`
   color: ${(props) => props.color};
   font-size: ${RFValue(8)}px;
-  font-family: ${theme.fonts.regular.fontFamily};
+  font-family: ${(props) => props.fontFamily};
 `;
