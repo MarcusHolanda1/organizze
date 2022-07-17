62e8e7c9211dff5659678af154f61310f79e4325
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-07-17 20:32:48 -0300

    feat: adding prop fontfamily

--- src/design/components/Text/index.tsx
+++ src/design/components/Text/index.tsx
@@ -6,24 +6,46 @@ interface Props {
   children: any;
   type?: "h1" | "h3" | "h4" | "p" | "small";
   color?: string;
+  fontFamily?: string;
 }
 
 const Text = ({
   children,
   type = "p",
   color = theme.colors.background,
+  fontFamily = theme.fonts.regular.fontFamily,
 }: Props) => {
   switch (type) {
     case "h1":
-      return <S.H1 color={color}>{children}</S.H1>;
+      return (
+        <S.H1 fontFamily={fontFamily} color={color}>
+          {children}
+        </S.H1>
+      );
     case "h3":
-      return <S.H3 color={color}>{children}</S.H3>;
+      return (
+        <S.H3 fontFamily={fontFamily} color={color}>
+          {children}
+        </S.H3>
+      );
     case "h4":
-      return <S.H4 color={color}>{children}</S.H4>;
+      return (
+        <S.H4 fontFamily={fontFamily} color={color}>
+          {children}
+        </S.H4>
+      );
     case "small":
-      return <S.SMALL color={color}>{children}</S.SMALL>;
+      return (
+        <S.SMALL fontFamily={fontFamily} color={color}>
+          {children}
+        </S.SMALL>
+      );
     default:
-      return <S.P color={color}>{children}</S.P>;
+      return (
+        <S.P fontFamily={fontFamily} color={color}>
+          {children}
+        </S.P>
+      );
   }
 };
 export default Text;
