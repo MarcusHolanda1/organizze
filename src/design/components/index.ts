17fc014aa6a6222b3052a64ad4e51944f6d2e9d4
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-07-23 19:27:37 -0300

    feat: export new components

--- src/design/components/index.ts
+++ src/design/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Text } from "./Text";
 export { default as Icons } from "./Icons";
-export { default as CardHome } from "./Cards/CardHome";
+export { default as Card } from "./Card";
+export { default as PrimaryButton } from "./Buttons/Primary";
