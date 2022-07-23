b27d031a459a2a5e372323e94d249ab412cd2b47
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-07-23 19:29:06 -0300

    refactor: structure page editing

--- src/design/structures/Page/index.tsx
+++ src/design/structures/Page/index.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import * as S from "./styles";
 
 interface PageProps {
-  children: any;
+  children?: any;
+  isStack?: boolean;
+  stack?: any;
+  includesPadding?: boolean;
 }
 
-const Page = ({ children }: PageProps) => {
+const Page = ({
+  children,
+  isStack,
+  stack,
+  includesPadding = true,
+}: PageProps) => {
   return (
     <S.Container>
-      <S.Content>{children}</S.Content>
+      {isStack ? (
+        <>
+          <S.HeaderStack>{stack}</S.HeaderStack>
+          <S.Content includesPadding={includesPadding}>{children}</S.Content>
+        </>
+      ) : (
+        <S.Content includesPadding={includesPadding}>{children}</S.Content>
+      )}
     </S.Container>
   );
 };
--- src/design/structures/Page/styles.ts
+++ src/design/structures/Page/styles.ts
@@ -2,12 +2,23 @@ import styled from "styled-components/native";
 import { StatusBar, Platform } from "react-native";
 
 import { theme } from "../../../theme";
-import { RFValue } from "react-native-responsive-fontsize";
+import { RFValue, RFPercentage } from "react-native-responsive-fontsize";
+
+interface Props {
+  includesPadding?: boolean;
+}
 
 export const Container = styled.SafeAreaView`
   padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
 `;
 
-export const Content = styled.View`
-  padding: ${RFValue(theme.spacing.n16)}px;
+export const Content = styled.View<Props>`
+  padding: ${(props) =>
+    props.includesPadding ? RFValue(theme.spacing.n16) : 0}px;
+  height: ${RFPercentage(95)}px;
+`;
+
+export const HeaderStack = styled.View`
+  width: 100%;
+  padding: ${RFValue(theme.spacing.n8)}px;
 `;
