c6a063cdd0b152d5319ebe8d628a3a3fb4ab31e3
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-07-24 17:42:37 -0300

    feat: export new components

--- src/design/components/index.ts
+++ src/design/components/index.ts
@@ -2,3 +2,7 @@ export { default as Text } from "./Text";
 export { default as Icons } from "./Icons";
 export { default as Card } from "./Card";
 export { default as PrimaryButton } from "./Buttons/Primary";
+export { default as BottomSheet } from "./BottomSheet";
+export { default as ContainerInput } from "./ContainerInput";
+export { default as IconButton } from "./Buttons/IconButton";
+export { default as DynamicSvg } from "./DynamicSvg";
